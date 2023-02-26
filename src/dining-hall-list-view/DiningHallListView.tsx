7575a9137e1dabd3a7bb8a809a4ee10c0f34966c
weitcis <weitcis@pm.me>
2023-02-26 11:24:57 -0800

    add: list view mode in DiningHallListView

--- src/dining-hall-list-view/DiningHallListView.tsx
+++ src/dining-hall-list-view/DiningHallListView.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, Text, View, Image, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -13,10 +13,16 @@ const styles = StyleSheet.create({
 	hstack: {
 		flexDirection: 'row',
 		alignItems: 'center',
+		padding: 8,
 	},
-	image: {
+	iconimage: {
 		width: 120,
 		height: 120,
+		borderRadius: 16
+	},
+	listimage: {
+		width: 80,
+		height: 80,
 		borderRadius: 16
 	},
 	subcategoryName: {
@@ -29,21 +35,58 @@ const styles = StyleSheet.create({
 })
 
 export default function DiningHallListView() {
+	const [useListView, setView] = useState(true)
 	return <>
 		<FlatList data={[1, 2, 3]} renderItem={({ item }) =>
 			<View
 				style={styles.cell}>
 				<Text style={styles.subcategoryName}>Subcategory</Text>
-				<FlatList data={[1, 1, 1, 1, 1, 1, 1, 1]} horizontal={true} renderItem={({ item }) =>
-					<View style={styles.vstack}>
-						<Image
-							source={require("../../assets/logo.png")}
-							style={styles.image}
-						/>
-						<Text>Test</Text>
-					</View>
-				} />
+				<Dishes listMode={useListView} />
 			</View>
 		} />
 	</>
+}
+
+type DishProps = {
+	listMode: boolean,
+}
+
+function Dishes(props: DishProps) {
+	if (props.listMode) {
+		return ListView()
+	} else {
+		return IconsView()
+	}
+}
+
+function IconsView() {
+	return <FlatList data={[1, 1, 1, 1, 1, 1, 1, 1]} horizontal={true} renderItem={({ item }) =>
+		<View style={styles.vstack}>
+			<Image
+				source={require("../../assets/logo.png")}
+				style={styles.iconimage}
+			/>
+			<Text>Test</Text>
+		</View>
+	} />
+}
+
+function ListView() {
+	return <FlatList data={[1, 1, 1, 1, 1, 1, 1, 1]} renderItem={({ item }) =>
+		<View style={styles.hstack}>
+			<Image
+				source={require("../../assets/logo.png")}
+				style={styles.listimage}
+			/>
+			<View style={styles.vstack}>
+				<Text>Tator Tots</Text>
+				<TagsView />
+			</View>
+			<Text>Omar is going to hell for lying that there is tatot tots at BPlate</Text>
+		</View>
+	} />
+}
+
+function TagsView() {
+	return <></>
 }
