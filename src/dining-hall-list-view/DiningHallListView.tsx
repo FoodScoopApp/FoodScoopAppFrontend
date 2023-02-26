1c1a5edd2424aba8cc92b41e4f15d7e6d40a60a5
weitcis <weitcis@pm.me>
2023-02-26 13:58:38 -0800

    add: interface for data input in DiningHallListView

--- src/dining-hall-list-view/DiningHallListView.tsx
+++ src/dining-hall-list-view/DiningHallListView.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { FlatList, Text, View, Image, StyleSheet } from 'react-native';
+import { FlatList, Text, View, Image, StyleSheet, ImageSourcePropType } from 'react-native';
+import { DietaryRestriction, DiningHall, DiningHallName, Meal, MealID, MealPeriod, MealPeriodName } from '../dataconnection/FoodScoopAppTypes/models';
 
 const styles = StyleSheet.create({
 	cell: {
@@ -9,11 +10,13 @@ const styles = StyleSheet.create({
 		flexDirection: 'column',
 		alignItems: 'center',
 		padding: 8,
+		gap: 8,
 	},
 	hstack: {
 		flexDirection: 'row',
 		alignItems: 'center',
 		padding: 8,
+		width: "100%"
 	},
 	iconimage: {
 		width: 120,
@@ -34,59 +37,128 @@ const styles = StyleSheet.create({
 	}
 })
 
-export default function DiningHallListView() {
+type DiningHallListProps = {
+	diningHallName: DiningHallName
+}
+
+function getDiningHall(name: DiningHallName): DiningHall | null {
+	// TODO: implementation
+	return null
+}
+
+function getPeriod(hall: DiningHall): MealPeriod | null {
+	// TODO: implementation
+	return null
+}
+
+function getMeal(id: MealID): Meal | null {
+	// TODO: implementation
+	return null
+}
+
+function getImage(id: MealID): ImageSourcePropType | null {
+	// TODO: implementation
+	return null
+}
+
+export default function DiningHallListView(props: DiningHallListProps) {
 	const [useListView, setView] = useState(true)
+	const diningHall = getDiningHall(props.diningHallName)
+	if (diningHall == null) {
+		return <></>
+	}
+	const currentPeriod = getPeriod(diningHall)
+	if (currentPeriod == null) {
+		return <></>
+	}
 	return <>
-		<FlatList data={[1, 2, 3]} renderItem={({ item }) =>
+		<FlatList data={currentPeriod.subcategories} renderItem={({ item }) =>
 			<View
 				style={styles.cell}>
 				<Text style={styles.subcategoryName}>Subcategory</Text>
-				<Dishes listMode={useListView} />
+				<Dishes listMode={useListView} meals={item.meals} />
 			</View>
 		} />
 	</>
 }
 
-type DishProps = {
+type MealsViewProps = {
 	listMode: boolean,
+	meals: MealID[],
 }
 
-function Dishes(props: DishProps) {
+function Dishes(props: MealsViewProps) {
 	if (props.listMode) {
-		return ListView()
+		return ListView(props)
 	} else {
-		return IconsView()
+		return IconsView(props)
 	}
 }
 
-function IconsView() {
-	return <FlatList data={[1, 1, 1, 1, 1, 1, 1, 1]} horizontal={true} renderItem={({ item }) =>
+function IconsView(props: MealsViewProps) {
+	return <FlatList data={props.meals} horizontal={true} renderItem={({ item }) =>
+		<IconItem meal={item} />
+	} />
+}
+
+function IconItem(props: MealItemProps) {
+	const meal = getMeal(props.meal)
+	const image = getImage(props.meal)
+	if (meal == null) {
+		return <></>
+	}
+	if (image == null) {
+		return <></>
+	}
+	return <View style={styles.vstack}>
+		<Image
+			source={image}
+			style={styles.iconimage}
+		/>
+		<Text>{meal.name}</Text>
+	</View>
+}
+
+function ListView(props: MealsViewProps) {
+	return <FlatList data={props.meals} renderItem={({ item }) =>
+		<ListItem meal={item} />
+	} />
+}
+
+type MealItemProps = {
+	meal: MealID,
+}
+
+function ListItem(props: MealItemProps) {
+	const meal = getMeal(props.meal)
+	const image = getImage(props.meal)
+	if (meal == null) {
+		return <></>
+	}
+	if (image == null) {
+		return <></>
+	}
+	return <View style={styles.hstack}>
+		<Image
+			source={image}
+			style={styles.listimage}
+		/>
 		<View style={styles.vstack}>
-			<Image
-				source={require("../../assets/logo.png")}
-				style={styles.iconimage}
-			/>
-			<Text>Test</Text>
+			<Text>{meal.name}</Text>
+			<TagsView restrictions={meal.dietaryRestrictions} />
 		</View>
-	} />
+		<Text>{
+			meal.description
+			// TODO: handle undefined
+		}</Text>
+	</View>
 }
 
-function ListView() {
-	return <FlatList data={[1, 1, 1, 1, 1, 1, 1, 1]} renderItem={({ item }) =>
-		<View style={styles.hstack}>
-			<Image
-				source={require("../../assets/logo.png")}
-				style={styles.listimage}
-			/>
-			<View style={styles.vstack}>
-				<Text>Tator Tots</Text>
-				<TagsView />
-			</View>
-			<Text>Omar is going to hell for lying that there is tatot tots at BPlate</Text>
-		</View>
-	} />
+type RestrictionTagsProps = {
+	restrictions: DietaryRestriction[]
 }
 
-function TagsView() {
-	return <></>
+function TagsView(props: RestrictionTagsProps) {
+	// implementation
+	return <Text>Nothing yet.</Text>
 }
