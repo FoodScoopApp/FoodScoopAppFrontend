9ce90b0258eba8d0a802a89bc70e77673ce4359d
Arya Gharib <40894911+Aryakhan81@users.noreply.github.com>
2023-02-26 14:05:25 -0800

    Comment out components we are no longer using

--- src/auth/SignupScreen.tsx
+++ src/auth/SignupScreen.tsx
@@ -1,72 +1,132 @@
 import React, {useState} from "react";
-import {Button, Dimensions, ScrollView, StyleSheet, Text, TextInput, View} from "react-native";
+import {Button, Dimensions, Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
 import {MultipleSelectList, SelectList} from "react-native-dropdown-select-list/index";
+import {LinearGradient} from "expo-linear-gradient";
 
 export default function SignupScreen({ navigation } : {navigation : any}) {
     const [name, setName] = useState("");
-    const [mealPlan, setMealPlan] = useState("");
-    const [restrictions, setRestrictions] = useState([])
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    const meal_plans = [
-        {key:'1', value: "11R"},
-        {key:'2', value: "11P"},
-        {key:'3', value: "14R"},
-        {key:'4', value: "14P"},
-        {key:'5', value: "19R"},
-        {key:'6', value: "19P"},
-    ]
+    function createAccount() {
+        //Do account creation stuff here
+        navigation.navigate("HomeScreen")
+    }
 
-    const diet_restrict = [
-        {key:'V', value: "Vegetarian"},
-        {key:'VG', value: "Vegan"},
-        {key:'HL', value: "Halal"},
-        {key:'LC', value: "Low Carbon"},
-        {key:'HC', value: "High Carbon"},
-        {key:'APNT', value: "Peanut Allergy"},
-        {key:'ATNT', value: "Tree Nut Allergy"},
-        {key:'AWHT', value: "Wheat Allergy"},
-        {key:'AGTN', value: "Gluten Allergy"},
-        {key:'ASOY', value: "Soy Allergy"},
-        {key:'ASES', value: "Sesame Allergy"},
-        {key:'AMLK', value: "Milk Allergy"},
-        {key:'AEGG', value: "Egg Allergy"},
-        {key:'ACSF', value: "Shellfish Allergy"},
-        {key:'AFSH', value: "Fish Allergy"}
-    ]
+    // const [mealPlan, setMealPlan] = useState("");
+    // const [restrictions, setRestrictions] = useState([])
+
+    // const meal_plans = [
+    //     {key:'1', value: "11R"},
+    //     {key:'2', value: "11P"},
+    //     {key:'3', value: "14R"},
+    //     {key:'4', value: "14P"},
+    //     {key:'5', value: "19R"},
+    //     {key:'6', value: "19P"},
+    // ]
+    //
+    // const diet_restrict = [
+    //     {key:'V', value: "Vegetarian"},
+    //     {key:'VG', value: "Vegan"},
+    //     {key:'HL', value: "Halal"},
+    //     {key:'LC', value: "Low Carbon"},
+    //     {key:'HC', value: "High Carbon"},
+    //     {key:'APNT', value: "Peanut Allergy"},
+    //     {key:'ATNT', value: "Tree Nut Allergy"},
+    //     {key:'AWHT', value: "Wheat Allergy"},
+    //     {key:'AGTN', value: "Gluten Allergy"},
+    //     {key:'ASOY', value: "Soy Allergy"},
+    //     {key:'ASES', value: "Sesame Allergy"},
+    //     {key:'AMLK', value: "Milk Allergy"},
+    //     {key:'AEGG', value: "Egg Allergy"},
+    //     {key:'ACSF', value: "Shellfish Allergy"},
+    //     {key:'AFSH', value: "Fish Allergy"}
+    // ]
 
     return(
         <ScrollView>
-            <View style={styles.nameView}>
-                <Text style={styles.text}>Name:</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder={"name"}
-                    placeholderTextColor={"grey"}
-                    value={name}
-                    onChangeText={setName}/>
+            <View style={styles.logoView}>
+                <Image
+                    source={require("../../assets/logo.png")}
+                    style={styles.image}/>
+                <Text style={styles.titleText}>FoodScoop</Text>
             </View>
-            <SelectList
-                boxStyles={styles.selectList}
-                setSelected={(val: any) => setMealPlan(val)}
-                data={meal_plans}
-                save="value"
-                search={false}
-                placeholder={"Select Meal Plan"}
-            />
-            <MultipleSelectList
-                boxStyles={styles.selectList}
-                setSelected={(val: any) => setRestrictions(val)}
-                data={diet_restrict}
-                save="value"
-                onSelect={() => alert(restrictions)}
-                label="Categories"
-                placeholder={"Select Dietary Restrictions"}
-            />
+            <TextInput
+                style={styles.input}
+                placeholder={"email"}
+                placeholderTextColor={"grey"}
+                value={email}
+                onChangeText={setEmail}/>
+            <TextInput
+                style={styles.input}
+                placeholder={"name"}
+                placeholderTextColor={"grey"}
+                value={name}
+                onChangeText={setName}/>
+            <TextInput
+                style={styles.input}
+                placeholder={"password"}
+                placeholderTextColor={"grey"}
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry={true}/>
+
+            {/*<View style={styles.nameView}>*/}
+            {/*    <Text style={styles.text}>Name:</Text>*/}
+            {/*    <TextInput*/}
+            {/*        style={styles.input}*/}
+            {/*        placeholder={"name"}*/}
+            {/*        placeholderTextColor={"grey"}*/}
+            {/*        value={name}*/}
+            {/*        onChangeText={setName}/>*/}
+            {/*</View>*/}
+            {/*<SelectList*/}
+            {/*    boxStyles={styles.selectList}*/}
+            {/*    setSelected={(val: any) => setMealPlan(val)}*/}
+            {/*    data={meal_plans}*/}
+            {/*    save="value"*/}
+            {/*    search={false}*/}
+            {/*    placeholder={"Select Meal Plan"}*/}
+            {/*/>*/}
+            {/*<MultipleSelectList*/}
+            {/*    boxStyles={styles.selectList}*/}
+            {/*    setSelected={(val: any) => setRestrictions(val)}*/}
+            {/*    data={diet_restrict}*/}
+            {/*    save="value"*/}
+            {/*    onSelect={() => alert(restrictions)}*/}
+            {/*    label="Categories"*/}
+            {/*    placeholder={"Select Dietary Restrictions"}*/}
+            {/*/>*/}
+            <LinearGradient
+                colors={["#DE6437", "#D93C78"]}
+                style={styles.loginButton}>
+                <TouchableOpacity onPress={() => createAccount()}>
+                    <Text style={{fontSize: 16}}>Create Account!</Text>
+                </TouchableOpacity>
+            </LinearGradient>
         </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
+    image: {
+        width: 80,
+        height: 80,
+        borderRadius: 16
+    },
+    titleText: {
+        fontFamily: "Avenir",
+        fontSize: 40,
+        marginLeft: 20
+    },
+    logoView: {
+        alignItems: "center",
+        alignSelf: "center",
+        flexDirection: "row",
+        width: Dimensions.get("screen").width - 50,
+        paddingTop: Dimensions.get("screen").height / 2 - 250,
+        paddingBottom: 40
+    },
     nameView: {
         flexDirection: "row",
         justifyContent: "space-between",
@@ -81,7 +141,6 @@ const styles = StyleSheet.create({
     input: {
         alignSelf: "stretch",
         flexGrow: 9,
-        // minWidth: "80%",
         height: 40,
         margin: 12,
         marginHorizontal: 30,
@@ -91,5 +150,13 @@ const styles = StyleSheet.create({
     selectList: {
         marginHorizontal: 30,
         marginVertical: 10
+    },
+    loginButton: {
+        height: 40,
+        margin: 12,
+        marginHorizontal: 30,
+        padding: 10,
+        alignItems: "center",
+        borderRadius: 10,
     }
 })
