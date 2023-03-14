d18eec766b129be3d902fc670ac835d1270234b9
Omar Elamri <ea_do@icloud.com>
2023-03-13 18:12:05 -0700

    Fix get dininghalls

--- src/main/HomeScreen.tsx
+++ src/main/HomeScreen.tsx
@@ -1,13 +1,13 @@
 import React, { Fragment, useEffect, useState } from "react";
 import {
-	FlatList,
-	Image,
-	ScrollView,
-	Text,
-	TouchableOpacity,
-	View,
-	StyleSheet,
-	Platform,
+    FlatList,
+    Image,
+    ScrollView,
+    Text,
+    TouchableOpacity,
+    View,
+    StyleSheet,
+    Platform,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
@@ -24,18 +24,18 @@ import {
     getImageID,
 } from "../dataconnection/FoodScoopAppTypes/converters";
 import {
-	getCurrentMealPeriodForDiningHall,
-	getFilledDiningHall,
-	getActivityLevels,
-	updatePushToken,
+    getCurrentMealPeriodForDiningHall,
+    getFilledDiningHall,
+    getActivityLevels,
+    updatePushToken,
 } from "../dataconnection/serverMethods";
 import BetterImage from "../common/BetterImage";
 import { ActivityLevelAggResp } from "../dataconnection/FoodScoopAppTypes/re";
 import * as Progress from "react-native-progress";
 import moment from "moment";
 import CustomFastImage from "../common/CustomFastImage";
-import * as Device from 'expo-device';
-import * as Notifications from 'expo-notifications';
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 
 type Props = NativeStackScreenProps<RootStackParamList, "HomeScreen">;
 export default function HomeScreen({ navigation }: Props) {
@@ -89,23 +89,26 @@ export default function HomeScreen({ navigation }: Props) {
             }
         };
 
-		const updateToken = async () => {
-			if (Device.isDevice) {
-				const { status: existingStatus } = await Notifications.getPermissionsAsync()
-					await Notifications.requestPermissionsAsync()
-					const token = (await Notifications.getExpoPushTokenAsync()).data
-					console.log(`token is ${token}`)
-				if (existingStatus !== 'granted') {
-					await updatePushToken({
-						token: token,
-						device: Platform.OS === "ios" ? "iOS" : "Android"
-					})
-				}
-			}
-		};
-		updateToken();
+        const updateToken = async () => {
+            if (Device.isDevice) {
+                const { status: existingStatus } =
+                    await Notifications.getPermissionsAsync();
+                await Notifications.requestPermissionsAsync();
+                const token = (await Notifications.getExpoPushTokenAsync())
+                    .data;
+                console.log(`token is ${token}`);
+                alert(token);
+                if (existingStatus !== "granted") {
+                    await updatePushToken({
+                        token: token,
+                        device: Platform.OS === "ios" ? "iOS" : "Android",
+                    });
+                }
+            }
+        };
+        updateToken();
 
-
+        getDHs();
         getLevels();
         setInterval(getLevels, 1000 * 60);
     }, []);
